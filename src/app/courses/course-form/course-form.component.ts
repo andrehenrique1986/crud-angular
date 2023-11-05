61ac366f7644dcf407a66a9f0226eee06194e7e9
andrehenrique1986 <andrehenriquelima1986@gmail.com>
2023-11-05 00:12:17 -0300

    API Spring: Criar Curso (HTTP POST)

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -15,7 +16,8 @@ form: FormGroup;
 
   constructor(private formBuilder: FormBuilder,
               private service: CoursesService,
-              private snackBar: MatSnackBar){
+              private snackBar: MatSnackBar,
+              private location: Location){
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
@@ -29,16 +31,21 @@ form: FormGroup;
 
   onSubmit(){
     this.service.save(this.form.value)
-    .subscribe(result => console.log(result), error => this.onError());
+    .subscribe(result => this.OnSuccess(), error => this.onError());
 
   }
 
   onCancel(){
+    this.location.back();
+  }
 
+  private OnSuccess(){
+    this.snackBar.open('Curso Salvo com sucesso !','',{duration: 5000});
+    this.onCancel();
   }
 
   private onError(){
-    this.snackBar.open('Erro ao salvar curso','',{duration: 5000});
+    this.snackBar.open('Erro ao salvar curso.','',{duration: 5000});
   }
 
 
